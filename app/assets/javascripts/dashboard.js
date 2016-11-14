e8c3282785fd189259ee2755d578272e7220a369
Rick <rick.eihara@gmail.com>
2016-11-14 19:45:37 -0200

    Integrated all css and js files to rails (gentella)

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,26 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require Chart.js/dist/Chart.min
+//= require gauge.js/dist/gauge.min
+//= require bootstrap-progressbar/bootstrap-progressbar.min
+//= require iCheck/icheck.min
+//= require skycons/skycons
+//= require Flot/jquery.flot
+//= require Flot/jquery.flot.pie
+//= require Flot/jquery.flot.time
+//= require Flot/jquery.flot.stack
+//= require Flot/jquery.flot.resize
+//= require flot.orderbars/js/jquery.flot.orderBars
+//= require flot-spline/js/jquery.flot.spline.min
+//= require flot.curvedlines/curvedLines
+//= require DateJS/build/date
+//= require jqvmap/dist/jquery.vmap
+//= require jqvmap/dist/maps/jquery.vmap.world
+//= require jqvmap/examples/js/jquery.vmap.sampledata
+//= require moment/min/moment.min
+//= require bootstrap-daterangepicker/daterangepicker
+
