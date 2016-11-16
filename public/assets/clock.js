15b8f9b997ae3b76262317b6c2c27ce6fa0d97f1
Rick <rick.eihara@gmail.com>
2016-11-15 22:24:13 -0200

    Forgot to add these new js files

---
+++ public/assets/clock.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    // init
+    var m = moment();
+    var minutes = (m.hour() * 60) + m.minute();
+    var progresshour = 0;
+    var clockHeight = $('#clock').height();
+    var viewportHeight = window.innerHeight;
+    var scrollbarHeight = viewportHeight / $(document).height() * viewportHeight;
+
+    console.log(clockHeight);
+
+    function posLayout(scrollbarHeight) {
+        $('.schedule').css('margin-top', scrollbarHeight / 2);
+        $('.schedule').css('margin-bottom', scrollbarHeight / 2);
+    }
+
+    function progressToText(progress, ratio, modulo, addZero) {
+        const val = modulo ? Math.floor(progress * ratio) % modulo : Math.floor(progress * ratio);
+        return (val > 9 && addZero) ? val.toString() : "0" + val.toString();
+    }
+
+    function translateElement(id, val) {
+        $(id).css({
+            transform: 'translateY(' + val + 'px)',
+            '-webkit-transform': 'translateY(' + val + 'px)',
+            '-ms-transform': 'translateY(' + val + 'px)'
+        });
+    }
+
+    function updateUI(distanceCircle, progress) {
+        $('.digital-time').text('' + progressToText(progress, (1440 / 60), null, true) + ':' + progressToText(progress, 1440, 60, true) + '');
+        translateElement('#clock', distanceCircle);
+    }
+
+    function scrollClock(e) {
+        const documentHeight = $(document).height();
+
+        var viewportHeight = window.innerHeight;
+        var scrollbarHeight = viewportHeight / documentHeight * viewportHeight;
+        var progress = $(window).scrollTop() / (documentHeight - viewportHeight);
+        var distanceCircle = progress * (viewportHeight - scrollbarHeight) + scrollbarHeight / 2 - clockHeight/2;
+
+        //functions block
+        posLayout(scrollbarHeight);
+        updateUI(distanceCircle, progress);
+
+    };
+
+    // main
+    posLayout(scrollbarHeight);
+    window.scrollTo(0, (minutes / 1440) * ($(document).height() - window.innerHeight));
+
+    $(window).scroll(_.throttle(function() {
+        scrollClock();
+    }, 80));
+});
+
