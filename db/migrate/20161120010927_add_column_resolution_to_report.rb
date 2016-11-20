db1e9fdd6e76777e5179bc2a50f51f6270ae0157
Rick <japa.eihara@gmail.com>
2016-11-20 04:20:00 -0200

    Fixed initial state for a report

--- db/migrate/20161120010927_add_column_resolution_to_report.rb
+++ db/migrate/20161120010927_add_column_resolution_to_report.rb
@@ -1,5 +1,5 @@
 class AddColumnResolutionToReport < ActiveRecord::Migration[5.0]
   def change
-    add_column :reports, :resolution, :string
+    add_column :reports, :resolution, :string, :default => "pending"
   end
 end
