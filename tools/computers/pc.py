bf67574f7f81f971ca638ddad7242ecb58b7960d
Henrique Eihara <rick.eihara@gmail.com>
2016-12-06 19:58:44 -0200

    fixed python script

--- tools/computers/pc.py
+++ tools/computers/pc.py
@@ -3,9 +3,12 @@ import sys
 import time
 
 while True:
-    params = urllib.urlencode({'computer_id':sys.argv[1:][0], 'status': "available"})
-    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-    conn = httplib.HTTPSConnection("siglaufscar.herokuapp.com")
-    conn.request("POST", "/LSO/status", params, headers)
-    response = conn.getresponse()
+    try:
+        params = urllib.urlencode({'computer_id':sys.argv[1:][0], 'status': "available"})
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPSConnection("siglaufscar.herokuapp.com")
+        conn.request("POST", "/LSO/status", params, headers)
+        response = conn.getresponse()
+    except Exception as e:
+        continue
     time.sleep(300)
