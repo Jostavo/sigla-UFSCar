0c3fa20e669906ff6dcb7e66ed58e2723dea06b4
Renan Lordello <renan.lord.de.aguiar@gmail.com>
2016-12-10 19:46:23 -0200

    cadastrador de digital wip

---
+++ tools/biometrics/biometrics/biometrics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  biometrics
+//
+//  Created by Renan Lordello de Aguiar on 11/11/16.
+//  Copyright © 2016 Golden Avocado Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
