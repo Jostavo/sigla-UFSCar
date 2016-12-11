7de635c2d7e2a4412eff9886bdf688ae22f9cac1
Henrique Eihara <rick.eihara@gmail.com>
2016-12-11 17:01:10 -0200

    Created a route and a action in authorized person to get all biometric data from a specific laboratory

--- app/controllers/authorized_person_controller.rb
+++ app/controllers/authorized_person_controller.rb
@@ -1,4 +1,13 @@
 class AuthorizedPersonController < ApplicationController
+ skip_before_action :verify_authenticity_token, only: [:get]
+
+  def get
+    respond_to do |format|
+      @person_biometric = AuthorizedPerson.where(authorized_person_params_get).select(:user_id, :biometric)
+      format.json { render json: @person_biometric }
+    end
+  end
+
   def save
     @authorized_person = AuthorizedPerson.new(authorized_person_params)
     if @authorized_person.save
@@ -15,6 +24,11 @@ class AuthorizedPersonController < ApplicationController
     AuthorizedPerson.where(:user_id => params_delete[:user_id]).where(:laboratory_id => params_delete[:laboratory_id])[0].delete
     flash.notice = "Autorização revogada!"
     redirect_to :back
+  end
+
+  private
+  def authorized_person_params_get
+    params.permit(:laboratory_id)
   end
 
   private
