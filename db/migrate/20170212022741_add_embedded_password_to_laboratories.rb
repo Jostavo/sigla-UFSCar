3051a7266678baf092ca24dfa7d8ebd74da9c4fd
Henrique <rick.eihara@gmail.com>
2017-02-12 00:33:40 -0200

    new name to password

--- db/migrate/20170212022741_add_embedded_password_to_laboratories.rb
+++ db/migrate/20170212022741_add_embedded_password_to_laboratories.rb
@@ -1,5 +1,5 @@
 class AddEmbeddedPasswordToLaboratories < ActiveRecord::Migration[5.0]
   def change
-    add_column :laboratories, :password, :string
+    add_column :laboratories, :embedded_password, :string
   end
 end
