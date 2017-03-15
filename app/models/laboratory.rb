f7902bf5ae523d230be156892e79502c4000fc07
Henrique <rick.eihara@gmail.com>
2017-03-14 23:50:11 -0300

    Changed controller to add new restrictions from embedded password

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,15 +1,18 @@
 class Laboratory < ApplicationRecord
+
   validates :title, presence: true
   validates :mantainer, presence: true
   validates :email, presence: true
   validates :initials, presence: true, uniqueness: true
   validates :embedded_password, uniqueness: true, presence: true
 
+  # if this columns is set to true, the embedded needs to update
+  #validates :embedded_update, presence: true
+
   has_many :status, dependent: :destroy
   has_many :subjects, dependent: :destroy
   has_many :computers, dependent: :destroy
   has_many :reports, dependent: :destroy
-
 
   # authorized person to access the laboratory
   has_many :authorized_person
