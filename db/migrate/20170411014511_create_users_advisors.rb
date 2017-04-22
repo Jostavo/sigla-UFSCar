3c577255928f0140c940b0832a6f1a2ca23e5908
Henrique Eihara <rick.eihara@gmail.com>
2017-04-22 13:44:16 -0300

    changes in migration

--- db/migrate/20170411014511_create_users_advisors.rb
+++ db/migrate/20170411014511_create_users_advisors.rb
@@ -3,7 +3,6 @@ class CreateUsersAdvisors < ActiveRecord::Migration[5.0]
     create_table :users_advisors do |t|
       t.references :professor, foreign_key: true, references: :user
       t.references :student, foreign_key: true, references: :user
-
       t.timestamps
     end
   end
