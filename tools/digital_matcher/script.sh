0e1679bbf02c4b312493870786985f72d7b4d8f2
Henrique Eihara <rick.eihara@gmail.com>
2017-05-10 23:20:26 -0300

    Some changes

--- tools/digital_matcher/script.sh
+++ tools/digital_matcher/script.sh
@@ -1,10 +1,10 @@
-!#/bin/bash
-
+#!/bin/bash
 cd ~/Documents
 mkdir git
+cd git
 
-sudo apt-get update
-sudo apt-get install git cmake libtoolize m4 automake libusb libnss3-dev libglib2.0-dev libx11-dev libxv-dev libcurl4-gnutls-dev
+apt-get update
+apt-get install git cmake m4 automake libusb-1.0-0-dev libnss3-dev libglib2.0-dev libx11-dev libxv-dev libcurl4-gnutls-dev libtool libpixman-1-dev libssl-dev -y
 
 git clone git://anongit.freedesktop.org/libfprint/libfprint
 
@@ -14,20 +14,23 @@ export LIBFPRINT_DEBUG=2
 ./autogen.sh
 ./configure
 make
-sudo make install
+make install
 
 cd ~/Documents/git
 git clone https://github.com/open-source-parsers/jsoncpp.git
+cd ./jsoncpp
 cmake .
 make
-sudo make install
+make install
 
 cd ~/Documents/git
 git clone https://github.com/Darkkgreen/trab-PDS.git
 cd ./trab-PDS/tools/digital_matcher/
 mkdir build
 cd build
-cmake .. -DEBUG=ON
+cmake .. -DDEBUG=ON
 make
-sudo make install
+make install
 
+cd ~/Documents
+rm -rf ./git
